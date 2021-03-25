4fb1c58608df333ac0bf8f8b2e45c55a0a909d82
detamos1398 <deepanshutyagi1398@gmail.com>
2021-03-25 21:34:25 +0530

    virutal env script for windows

---
+++ scripts/create_venv.bat
@@ -0,0 +1,19 @@
+
+
+echo "Checking %1 for virtual environment"
+if exist %1\ (
+  echo "Virtual environment already exists"
+) else (
+  echo "Creating virtual environment"
+  python3 -m venv %1
+  echo "Created the virtual environment"
+)
+
+echo "Activating the virtual environment"
+%1\Scripts\activate.bat
+
+if exist requirements.txt (
+  echo "Populating virtual environment using requirements.txt"
+  python3 -m pip install -r requirements.txt
+  echo "Successfully populated the virtual environment"
+)
